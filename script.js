8c08574650119680d19fddf03379b8c3d570dac4
princessjeanauburn <975wk6vf5h@privaterelay.appleid.com>
2026-09-09 15:24:33 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+const username = "YOUR_USERNAME";
+
+document.getElementById("github").innerHTML =
+  '<a href="https://github.com/' +
+  username +
+  '" target="_blank">@' +
+  username +
+  "</a>";
